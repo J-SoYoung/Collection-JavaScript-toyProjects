e4d63b63e127939cc545a722094a30051cfbfd34
thdud <thgml1444@naver.com>
2023-06-05 09:13:09 +0900

    [ ADD ] music-player 프로젝트를 생성하라

---
+++ music-player/src/App.js
@@ -0,0 +1,21 @@
+import "./App.scss";
+import Controls from "./components/Controls/Controls";
+import PlayList from "./components/PlayList/PlayList";
+import ProgressArea from "./components/ProgressArea/ProgressArea";
+import SongDetail from "./components/SongDetail/SongDetail";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <SongDetail />
+        <ProgressArea />
+        <Controls />
+        <PlayList />
+      </div>
+    </div>
+  );
+}
+
+export default App;
