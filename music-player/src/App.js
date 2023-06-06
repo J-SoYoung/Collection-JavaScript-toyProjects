13595ce66a04ab38a82bd6a4e8850cde088e7b0d
thdud <thgml1444@naver.com>
2023-06-07 08:43:06 +0900

    [ music-player-ADD ] progressBar를 구현하라 - html의 오디오 태그의 프로퍼티인 currentTime, duration을 활용하여 프로그래스바의 전체 시간, 현재시간을 width로 변형시켜 구현하였다. - 오디오 태그의 프로퍼티인 volume을 사용해 볼륨 조절도 구현하였다.

--- music-player/src/App.js
+++ music-player/src/App.js
@@ -6,9 +6,9 @@ import ProgressArea from "./components/ProgressArea/ProgressArea";
 import SongDetail from "./components/SongDetail/SongDetail";
 
 function App() {
-  // ProgressArea를 forwardRef로 감싸뒀기 때문에 
+  // ProgressArea를 forwardRef로 감싸뒀기 때문에
   // ProgressArea에 있는 audio와 관련된 메서드를 부.컴인 App에서 Ref를 통해 접근할 수 있게 됨.
-  // App컴포넌트는 ref에서 참조한 audio관련 메서드를 Controls에 props로 넘겨 사용할 수 있게 됨. 
+  // App컴포넌트는 ref에서 참조한 audio관련 메서드를 Controls에 props로 넘겨 사용할 수 있게 됨.
   const audioRef = useRef();
   const onPlay = () => {
     audioRef.current.play();
@@ -16,13 +16,16 @@ function App() {
   const onPause = () => {
     audioRef.current.pause();
   };
+  const changeVolume = (volume) => {
+    audioRef.current.changeVolume(volume);
+  };
 
   return (
     <div className="App">
       <div className="container">
         <SongDetail />
         <ProgressArea ref={audioRef} />
-        <Controls play={onPlay} pause={onPause} />
+        <Controls play={onPlay} pause={onPause} changeVolume={changeVolume} />
         <PlayList />
       </div>
     </div>
